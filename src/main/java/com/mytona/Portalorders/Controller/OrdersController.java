27b7514941ef929b0081c57c4f2238f5e7319adc
tengu <>
2021-01-15 16:41:47 +0200

    minor changes

--- src/main/java/com/mytona/Portalorders/Controller/OrdersController.java
+++ src/main/java/com/mytona/Portalorders/Controller/OrdersController.java
@@ -3,15 +3,16 @@ package com.mytona.Portalorders.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/orders")
 
 //TODO: create, confirm, update, delete orders
+
+@RestController
+@RequestMapping("/orders")
 public class OrdersController {
 
     @GetMapping("/{orderId}")
     public String getOrder(@PathVariable("orderId") String orderId) {
-        return "Successfully retrieved an order";
+        return "Successfully retrieved an order:" + " " + orderId;
     }
 
 }

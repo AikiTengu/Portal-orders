6f7387d3473bcf65d501e35f981b56b23936bb4c
tengu <aiki_tengu@yahoo.com>
2021-01-21 15:18:23 +0200

    consuming REST from payment and delivery

--- src/main/java/com/mytona/Portalorders/Model/Entity/Delivery.java
+++ src/main/java/com/mytona/Portalorders/Model/Entity/Delivery.java
@@ -1,4 +1,18 @@
 package com.mytona.Portalorders.Model.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class Delivery {
+    private long deliveryId;
+    private String deliveryType;
+    private LocalDate date;
+    private String address;
+    private boolean delivered;
 }
--- src/main/java/com/mytona/Portalorders/Model/Entity/Payment.java
+++ src/main/java/com/mytona/Portalorders/Model/Entity/Payment.java
@@ -1,4 +1,19 @@
 package com.mytona.Portalorders.Model.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class Payment {
+
+    private long paymentId;
+    private LocalDate date;
+    private short currencyType;
+    private double sum;
+
 }
